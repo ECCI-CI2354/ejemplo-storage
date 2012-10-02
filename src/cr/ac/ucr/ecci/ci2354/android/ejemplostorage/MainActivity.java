8a0fa7462a51b22b3010faff4c3973dce7de366f
fr4gus <fggarcia@gmail.com>
2012-10-01 22:44:31 -0600

    Agregando permisos

--- src/cr/ac/ucr/ecci/ci2354/android/ejemplostorage/MainActivity.java
+++ src/cr/ac/ucr/ecci/ci2354/android/ejemplostorage/MainActivity.java
@@ -1,15 +1,25 @@
 package cr.ac.ucr.ecci.ci2354.android.ejemplostorage;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	protected void onResume() {
+		File root = getExternalFilesDir(null);
+		Log.d("", root.getAbsolutePath());
+
+		super.onResume();
+	}
+
 }
